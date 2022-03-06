265d1bb796efef799647558bf98cc81a38e88630
thomas <thomas.lelievre@em-strasbourg.eu>
2022-03-06 16:51:23 +0100

    adding tests for the Euclidean distance

--- tests/testNuagePoints.hpp
+++ tests/testNuagePoints.hpp
@@ -1,6 +1,13 @@
 #pragma once
+
 #include <gtest/gtest.h>
 #include "NuagePoints.hpp"
+
+#include <iostream>
+#include <vector>
+#include <valarray>
+
+using namespace std;
 
 /******************************************************
  *                    NuagePoints                     *
@@ -8,5 +15,23 @@
 
 TEST(testNuagePoints, testDistanceEuclidienne)
 {
+    ASSERT_DOUBLE_EQ(
+        distance_euclidienne(
+                              vector({1., 2.}), 
+                              vector({2., 1.}) 
+                            ), 2.);
+
+    ASSERT_DOUBLE_EQ(
+        distance_euclidienne(
+                              valarray({1., 2., 1.}), 
+                              valarray({2., 1., 12.}) 
+                            ), 4.);
+
+    ASSERT_ANY_THROW(
+        distance_euclidienne(
+                             vector({1., 2.}), 
+                             vector({2., 1., 3.}) 
+                            )
+                                 );
 
 }
