9557d34ce5f5129c687fd1a12dd4e240fb1fa568
thomas <thomas.lelievre@em-strasbourg.eu>
2022-03-06 20:15:51 +0100

    Adding tests to NuagePoints

--- tests/testNuagePoints.hpp
+++ tests/testNuagePoints.hpp
@@ -35,3 +35,45 @@ TEST(testNuagePoints, testDistanceEuclidienne)
                                  );
 
 }
+
+TEST(testNuagePoints, test)
+{
+    
+}
+
+TEST(testNuagePoints, testGetDistance)
+{  
+
+}
+
+
+double dist(int a, int b){ return abs(b-a); }
+
+TEST(testNuagePoints, testOperatorPlus)
+{
+    int tab1[] = {1,2};
+    
+    NuagePoints<int> n(2, tab1, dist);
+    NuagePoints<int> n1(2, tab1, dist);
+    auto n2 = n + n1;
+
+    for (auto i = 0u; i < n2.size(); ++i)
+    {
+        ASSERT_EQ(n2[i], 1 + i%2);
+    }
+
+    auto tab2 = vector({valarray({1., 2., 1.}), 
+                        valarray({2., 1., 12.}) });
+    NuagePoints<valarray<double>> n3(2, tab2.data());
+    NuagePoints<valarray<double>> n4(2, tab2.data());
+    auto n5 = n3 + n4;
+    
+    for (auto i = 0u; i < n5.size(); ++i)
+    {
+        for (auto j = 0u; j < n5[i].size(); ++j)
+        {    
+            ASSERT_EQ(n5[i][j],  tab2[i%2][j]);
+        }
+    } 
+}
+
