53bd27405a9188a7d331844da365099c128ee70c
thomas <thomas.lelievre@em-strasbourg.eu>
2022-03-07 13:24:22 +0100

    first implementation of the Clusstering class

--- includes/Clustering.hpp
+++ includes/Clustering.hpp
@@ -12,15 +12,25 @@ private:
     int* mIdCluster;
 public:
     Clustering();
-    Clustering(int);
+    Clustering(int, NuagePoints<T>&);
     Clustering(const Clustering<T>&);
-    ~Clustering();
+    ~Clustering() { delete [] mIdCluster; }
 
     int& operator[](size_t i){ return mIdCluster[i]; }
-    virtual Clustering<T> operator=(const Clustering<T>&); 
+    virtual Clustering<T>& operator=(const Clustering<T>&); 
 
     int  get_nbClusters()     { return mNbClusters; }
-    void set_nbClusters(int n){   mNbClusters = n;  }
+    void set_nbClusters(int n)
+    {   
+        if (n>0) 
+        { 
+            mNbClusters = n; 
+        } 
+        else 
+        { 
+            throw invalid_argument("Number of Clusters should be positive.");
+        }
+    }
 
     NuagePoints<T>   get_nuage()  { return mNuage; } 
     void set_nuage(NuagePoints<T> N) { mNuage = N; }
@@ -31,25 +41,45 @@ public:
 template <class T> 
 Clustering<T>::Clustering()
 {
-
+    mNbClusters = 1;
+    mNuage = NuagePoints<T>();
+    mIdCluster = new int[0];
 }
 
 template <class T> 
-Clustering<T>::Clustering(int)
+Clustering<T>::Clustering(int nCl, NuagePoints<T>& N): mNbClusters(nCl), mNuage(N)
 {
-
+    mIdCluster = new int[mNuage.size()]
+    for (auto i = 0u; i < mNuage.size(); ++i)
+    {
+        mIdCluster[i] = -10;
+    }
 }
 
 template <class T> 
-Clustering<T>::Clustering(const Clustering<T>&)
+Clustering<T>::Clustering(const Clustering<T>& C)
 {
-
+    mNbClusters = C.mNbClusters;
+    mNuage = C.mNuage;
+    mIdCluster = new int[mNuage.size()]; 
+    for (auto i = 0u; i < mNuage.size(); ++i)
+    {
+        mIdCluster[i] = C.mIdCluster[i];
+    }
 }
 
 template <class T> 
-Clustering<T>::~Clustering()
+Clustering<T>& Clustering<T>::operator=(const Clustering<T>& C)
 {
-
+    mNbClusters = C.mNbClusters;
+    mNuage = C.mNuage;
+    auto tmp = mIdCluster;
+    mIdCluster = new int[mNuage.size()]; 
+    for (auto i = 0u; i < mNuage.size(); ++i)
+    {
+        mIdCluster[i] = C.mIdCluster[i];
+    }
+    delete [] tmp;
 }
 
 #endif
